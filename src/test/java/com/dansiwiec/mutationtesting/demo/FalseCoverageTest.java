e23bc09bf135daa8af6231ed995ce98b2ed0b971
Dan Siwiec <daniel.siwiec@gmail.com>
2017-11-02 09:11:14 -0700

    Add a comment explaining the poor test

--- src/test/java/com/dansiwiec/mutationtesting/demo/FalseCoverageTest.java
+++ src/test/java/com/dansiwiec/mutationtesting/demo/FalseCoverageTest.java
@@ -8,6 +8,7 @@ public class FalseCoverageTest {
 
     private FalseCoverage falseCoverage = new FalseCoverage();
 
+    // The test exercises the method, but doesn't make any assertions on the result. This yields a 100% coverage.
     @Test
     public void testAllowingTheMutant(){
         falseCoverage.complexProcess("foo", "bar");
